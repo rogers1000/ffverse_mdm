f36faecf0c0c1baf7077da8b5d4d7ebe015798b6
rogers1000 <89270891+rogers1000@users.noreply.github.com>
2022-01-20 21:38:17 +0000

    Add files via upload
    Added:
    - Random Rankers
    - Random Tactics
    
    Fixes:
    - TEs being overrated

--- FFverse_Mock_Draft_Machine_github.R
+++ FFverse_Mock_Draft_Machine_github.R
@@ -1,4 +1,9 @@
 
+## If not installed the following packages remove the "#"
+#install.packages(nflreadr)
+#install.packages(tidyverse)
+#install.pacakges(stringr)
+#install.packages(rlang)
 
 library(nflreadr)
 library(tidyverse)
@@ -8,7 +13,7 @@ library(rlang)
 options(scipen = 9999)
 
 ## Set WD if wanting to analyse and compare different mock drafts.
-#setwd("")
+setwd("C:\\Users\\zacro\\Documents\\Apprenticeship Work\\FFverse MDM\\v8")
 
 fantasy_ids <- load_ff_playerids() 
 
@@ -68,11 +73,11 @@ Team_Picks <- data.frame(Pick = c(1:Picks),Linear = c(1:Teams), Snake = c(1:Team
   mutate(Snake = ifelse(Snake == 12, "L",Snake)) %>%
   rename(Team = Snake)
 
-Count <- 466
+Count <- 0
 
 ### How Many Drafts do you want to make at once?
 ## Default is 1
-Simulations <- 500
+Simulations <- 10000
 
 ### Team Next Pick Default is League Size * 2
 Team_Next_Picks <- Teams*2
@@ -104,6 +109,37 @@ repeat{
            Ranker = ifelse(Ranker_Number == 7, "expertKevinCole",Ranker),
            Ranker = ifelse(Ranker_Number == 8, "expertBenBrown",Ranker))
   
+  A_Tactic <- sample(1:4,1)
+  B_Tactic <- sample(1:4,1)
+  C_Tactic <- sample(1:4,1)
+  D_Tactic <- sample(1:4,1)
+  E_Tactic <- sample(1:4,1)
+  F_Tactic <- sample(1:4,1)
+  G_Tactic <- sample(1:4,1)
+  H_Tactic <- sample(1:4,1)
+  I_Tactic <- sample(1:4,1)
+  J_Tactic <- sample(1:4,1)
+  K_Tactic <- sample(1:4,1)
+  L_Tactic <- sample(1:4,1)
+  
+  Team_Tactics <- data.frame(Team = c("A","B","C","D","E","F","G","H","I","J","K","L"), Tactic_Number = c(A_Tactic,B_Tactic,C_Tactic,D_Tactic,E_Tactic,F_Tactic,G_Tactic,H_Tactic,I_Tactic,J_Tactic,K_Tactic,L_Tactic)) %>%
+    mutate(Tactic = ifelse(Tactic_Number == 1, "WR0",Tactic_Number),
+           Tactic = ifelse(Tactic_Number == 2, "RB1",Tactic),
+           Tactic = ifelse(Tactic_Number == 3, "RB0",Tactic),
+           Tactic = ifelse(Tactic_Number == 4, "BPA",Tactic))
+  
+  
+  Roster_Rules <- data.frame(QB = 1, RB = 2, WR = 3, TE = 1, Flex = 1, SuperFlex = 0) %>%
+    mutate(RB = RB + Flex + SuperFlex) %>%
+    mutate(WR = WR + Flex + SuperFlex) %>%
+    mutate(TE = TE + Flex + SuperFlex) %>%
+    mutate(QB = QB + SuperFlex)
+  
+  RR_QBs <- dplyr::pull(Roster_Rules,QB)
+  RR_RBs <- dplyr::pull(Roster_Rules,RB)
+  RR_WRs <- dplyr::pull(Roster_Rules,WR)
+  RR_TEs <- dplyr::pull(Roster_Rules,TE)
+  
   Team_Next_Picks <- Teams*2
   Mock_Draft <- "None"
   player_position <- "None"
@@ -111,6 +147,7 @@ repeat{
   expected_player_name <- "None"
   expected_player_percent <- 0
   Picks_Drafted <- 0
+  Mock_Table <- data.frame(Team = c("A","B","C","D","E","F","G","H","I","J","K","L"), Name = "None",Position = "None")
   
 
   
@@ -131,11 +168,80 @@ repeat{
   
   Ranker <- dplyr::pull(Team_Ranker,Ranker)
   
+  On_clock_team <- Team_Picks %>%
+    filter(Pick > Picks_Drafted) %>%
+    mutate(Next_Pick = ifelse(Pick == Picks_Drafted + 1,1,0)) %>%
+    group_by(Team) %>%
+    summarise(On_the_clock = sum(Next_Pick)) %>%
+    ungroup() %>%
+    filter(On_the_clock == 1) 
+  
+  On_clock_team_vector <- dplyr::pull(On_clock_team,Team)
+  
+  TT_AfterR1 <- Mock_Table %>%
+    filter(Team == On_clock_team_vector) %>%
+    select(Team,Name,Position) %>%
+    mutate(QBs = ifelse(Position == "QB",1,0),
+           RBs = ifelse(Position == "RB",1,0),
+           WRs = ifelse(Position == "WR",1,0),
+           TEs = ifelse(Position == "TE",1,0)) %>%
+    group_by(Team) %>%
+    summarise(QBs = sum(QBs), RBs = sum(RBs), WRs = sum(WRs), TEs = sum(TEs)) %>%
+    mutate(QB_Starting_Spot = RR_QBs - QBs) %>%
+    mutate(RB_Starting_Spot = RR_RBs - RBs) %>%
+    mutate(WR_Starting_Spot = RR_WRs - WRs) %>%
+    mutate(TE_Starting_Spot = RR_TEs - TEs) %>%
+    #select(Team,QB_Starting_Spot,RB_Starting_Spot,WR_Starting_Spot,TE_Starting_Spot) %>%
+    left_join(Team_Tactics, by = "Team") %>%
+    mutate(Position_Target_WR0_a = ifelse(Tactic == "WR0" & RB_Starting_Spot > 0, "RB","")) %>%
+    mutate(Position_Target_WR0_b = ifelse(Tactic == "WR0" & RB_Starting_Spot < 1 & WR_Starting_Spot > 0, "TE_WR","")) %>%
+    mutate(Position_Target_WR0_c = ifelse(Tactic == "WR0" & RB_Starting_Spot < 1 & WR_Starting_Spot < 1, "ADP","")) %>%
+    mutate(Position_Target_RB0_a = ifelse(Tactic == "RB0" & WR_Starting_Spot > 0, "TE_WR","")) %>%
+    mutate(Position_Target_RB0_b = ifelse(Tactic == "RB0" & WR_Starting_Spot < 1 & RB_Starting_Spot > 0, "RB","")) %>%
+    mutate(Position_Target_RB0_c = ifelse(Tactic == "RB0" & WR_Starting_Spot < 1 & RB_Starting_Spot < 1, "ADP","")) %>%
+    mutate(Position_Target_RB1_a = ifelse(Tactic == "RB1" & RBs == 0, "RB", "")) %>%
+    mutate(Position_Target_RB1_b = ifelse(Tactic == "RB1" & RBs > 0, "TE_WR", "")) %>%
+    mutate(Position_Target_RB1_c = ifelse(Tactic == "RB1" & RB_Starting_Spot > 0 & WR_Starting_Spot < 1, "RB", "")) %>%
+    mutate(Position_Target_RB1_d = ifelse(Tactic == "RB1" & WR_Starting_Spot < 1 & RB_Starting_Spot < 1, "ADP","")) %>%
+    mutate(PT1 = ifelse(Position_Target_WR0_a == "RB","RB",Position_Target_WR0_b)) %>%
+    mutate(PT2 = ifelse(PT1 == "", Position_Target_WR0_c, PT1)) %>%
+    mutate(PT3 = ifelse(PT2 == "", Position_Target_RB0_a, PT2)) %>%
+    mutate(PT4 = ifelse(PT3 == "",Position_Target_RB0_b, PT3)) %>%
+    mutate(PT5 = ifelse(PT4 == "",Position_Target_RB0_c, PT4)) %>%
+    mutate(PT6 = ifelse(PT5 == "",Position_Target_RB1_a, PT5)) %>%
+    mutate(PT7 = ifelse(PT6 == "",Position_Target_RB1_b, PT6)) %>%
+    mutate(PT8 = ifelse(PT7 == "",Position_Target_RB1_c, PT7)) %>%
+    mutate(PT9 = ifelse(PT8 == "",Position_Target_RB1_d, PT8)) %>%
+    mutate(PT10 = ifelse(PT9 == "","ADP",PT9)) %>%
+    mutate(Tactic_Target = paste0(PT10,"_Score")) 
+    #select(Team,Tactic_Target) 
+  
+  Team_Target_afterR1 <- dplyr::pull(TT_AfterR1,Tactic_Target)
+  
+  TT_forR1 <- Team_Picks %>%
+    filter(Pick > Picks_Drafted) %>%
+    mutate(Next_Pick = ifelse(Pick == Picks_Drafted + 1,1,0)) %>%
+    group_by(Team) %>%
+    summarise(On_the_clock = sum(Next_Pick)) %>%
+    ungroup() %>%
+    filter(On_the_clock == 1) %>%
+    left_join(Team_Tactics, by = "Team") %>%
+    mutate(Team_Tactic = ifelse(Tactic == "RB0","TE_WR",""),
+           Team_Tactic = ifelse(Tactic == "RB1", "RB",Team_Tactic),
+           Team_Tactic = ifelse(Tactic == "WR0", "RB",Team_Tactic),
+           Team_Tactic = ifelse(Tactic == "BPA", "ADP",Team_Tactic)) %>%
+    mutate(Tactic_Target = paste0(Team_Tactic,"_Score")) 
+  
+  Team_Target_forR1 <- dplyr::pull(TT_forR1,Tactic_Target)
+  
+  Tactic_Vector <- ifelse(Picks_Drafted <= 12,Team_Target_forR1,Team_Target_afterR1)
+  
   
     Player_Selection <- all_ranks %>%
       filter(!name %in% Mock_Draft) %>%
-      group_by(position) %>%
-      mutate(QB_Rank = ifelse(position == "QB",rank(.data[[Ranker]]),0)) %>%
+      mutate(fantasy_pos = ifelse(position == "WR" | position == "TE", "WR", position)) %>%
+      group_by(fantasy_pos) %>%
+      mutate(QB_Rank = ifelse(fantasy_pos == "QB",rank(.data[[Ranker]]),0)) %>%
       mutate(QB_Score = ifelse(QB_Rank == 1, 50,0),
              QB_Score = ifelse(QB_Rank == 2, 40,QB_Score),
              QB_Score = ifelse(QB_Rank == 3, 30,QB_Score),
@@ -146,7 +252,7 @@ repeat{
              QB_Score = ifelse(QB_Rank == 8, 3,QB_Score),
              QB_Score = ifelse(QB_Rank == 9, 2,QB_Score),
              QB_Score = ifelse(QB_Rank == 10, 1,QB_Score)) %>%
-      mutate(RB_Rank = ifelse(position == "RB",rank(.data[[Ranker]]),0)) %>%
+      mutate(RB_Rank = ifelse(fantasy_pos == "RB",rank(.data[[Ranker]]),0)) %>%
       mutate(RB_Score = ifelse(RB_Rank == 1, 50,0),
              RB_Score = ifelse(RB_Rank == 2, 40,RB_Score),
              RB_Score = ifelse(RB_Rank == 3, 30,RB_Score),
@@ -157,28 +263,39 @@ repeat{
              RB_Score = ifelse(RB_Rank == 8, 3,RB_Score),
              RB_Score = ifelse(RB_Rank == 9, 2,RB_Score),
              RB_Score = ifelse(RB_Rank == 10, 1,RB_Score)) %>%
-      mutate(WR_Rank = ifelse(position == "WR",rank(.data[[Ranker]]),0)) %>%
-      mutate(WR_Score = ifelse(WR_Rank == 1, 50,0),
-             WR_Score = ifelse(WR_Rank == 2, 40,WR_Score),
-             WR_Score = ifelse(WR_Rank == 3, 30,WR_Score),
-             WR_Score = ifelse(WR_Rank == 4, 20,WR_Score),
-             WR_Score = ifelse(WR_Rank == 5, 10,WR_Score),
-             WR_Score = ifelse(WR_Rank == 6, 5,WR_Score),
-             WR_Score = ifelse(WR_Rank == 7, 4,WR_Score),
-             WR_Score = ifelse(WR_Rank == 8, 3,WR_Score),
-             WR_Score = ifelse(WR_Rank == 9, 2,WR_Score),
-             WR_Score = ifelse(WR_Rank == 10, 1,WR_Score)) %>%
-      mutate(TE_Rank = ifelse(position == "TE",rank(.data[[Ranker]]),0)) %>%
-      mutate(TE_Score = ifelse(TE_Rank == 1, 50,0),
-             TE_Score = ifelse(TE_Rank == 2, 40,TE_Score),
-             TE_Score = ifelse(TE_Rank == 3, 30,TE_Score),
-             TE_Score = ifelse(TE_Rank == 4, 20,TE_Score),
-             TE_Score = ifelse(TE_Rank == 5, 10,TE_Score),
-             TE_Score = ifelse(TE_Rank == 6, 5,TE_Score),
-             TE_Score = ifelse(TE_Rank == 7, 4,TE_Score),
-             TE_Score = ifelse(TE_Rank == 8, 3,TE_Score),
-             TE_Score = ifelse(TE_Rank == 9, 2,TE_Score),
-             TE_Score = ifelse(TE_Rank == 10, 1,TE_Score)) %>%
+      #mutate(WR_Rank = ifelse(fantasy_pos == "WR",rank(.data[[Ranker]]),0)) %>%
+      #mutate(WR_Score = ifelse(WR_Rank == 1, 50,0),
+      #       WR_Score = ifelse(WR_Rank == 2, 40,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 3, 30,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 4, 20,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 5, 10,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 6, 5,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 7, 4,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 8, 3,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 9, 2,WR_Score),
+      #       WR_Score = ifelse(WR_Rank == 10, 1,WR_Score)) %>%
+      #mutate(TE_Rank = ifelse(fantasy_pos == "TE",rank(.data[[Ranker]]),0)) %>%
+      #mutate(TE_Score = ifelse(TE_Rank == 1, 50,0),
+      #       TE_Score = ifelse(TE_Rank == 2, 40,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 3, 30,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 4, 20,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 5, 10,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 6, 5,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 7, 4,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 8, 3,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 9, 2,TE_Score),
+      #       TE_Score = ifelse(TE_Rank == 10, 1,TE_Score)) %>%
+    mutate(TE_WR_Rank = ifelse(fantasy_pos == "WR",rank(.data[[Ranker]]),0)) %>%
+      mutate(TE_WR_Score = ifelse(TE_WR_Rank == 1, 50,0),
+             TE_WR_Score = ifelse(TE_WR_Rank == 2, 40,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 3, 30,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 4, 20,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 5, 10,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 6, 5,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 7, 4,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 8, 3,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 9, 2,TE_WR_Score),
+             TE_WR_Score = ifelse(TE_WR_Rank == 10, 1,TE_WR_Score)) %>%
       ungroup() %>%
       mutate(ADP_Rank = rank(FFverse_OverallRank)) %>%
       mutate(ADP_Score = ifelse(ADP_Rank == 1, 50,0),
@@ -192,9 +309,9 @@ repeat{
              ADP_Score = ifelse(ADP_Rank == 9, 2,ADP_Score),
              ADP_Score = ifelse(ADP_Rank == 10, 1,ADP_Score)) %>%
       ### SELECT FUNCTION NEEDS UPDATING WHENEVER A NEW RANKER IS ADDED
-      select(ADP_Rank,name,position,FFverse_OverallRank,PFF_Consensus,expertAndrewErickson,expertNathanJahnke,expertIanHartitz,expertJaradEvans,expertDwainMcFarland,expertKevinCole,expertBenBrown,ADP_Score,QB_Score,RB_Score,WR_Score,TE_Score) %>%
-      mutate(Team_Strategy = ADP_Score) %>%
-      mutate(Total_Score = ADP_Score+QB_Score+RB_Score+WR_Score+TE_Score+Team_Strategy) %>%
+      select(ADP_Rank,name,position,FFverse_OverallRank,PFF_Consensus,expertAndrewErickson,expertNathanJahnke,expertIanHartitz,expertJaradEvans,expertDwainMcFarland,expertKevinCole,expertBenBrown,ADP_Score,QB_Score,RB_Score,TE_WR_Score) %>%
+      #Removed TE_Score and WR_Score from formula
+      mutate(Total_Score = ADP_Score+QB_Score+RB_Score+TE_WR_Score+.data[[Tactic_Vector]]) %>%
       mutate(Rank_Weighting_min = min(.data[[Ranker]])) %>%
       mutate(Rank_Weighting1 = (Rank_Weighting_min)/(.data[[Ranker]] + Rank_Weighting_min)) %>%
       mutate(Rank_Weighting2 = (Total_Score + Rank_Weighting1)*Rank_Weighting1) %>%
@@ -254,8 +371,9 @@ repeat{
       select(Pick,Team,Name,Position,Lottery,EP_Name,EP_Percent) %>%
       mutate(Round = ifelse(Pick%%Teams == 0, Pick/Teams,(Pick/Teams)+1)) %>%
       mutate(Round = floor(Round)) %>%
-      select(Round,Pick,Team,Name,Position,Lottery,EP_Name,EP_Percent) 
-    Picks_Drafted <- max(dplyr::pull(Mock_Table,Pick))
+      select(Round,Pick,Team,Name,Position,Lottery,EP_Name,EP_Percent)
+    
+    Picks_Drafted <- max(dplyr::pull(Mock_Table,Pick)) 
     
     Team_Next_Pick <- Team_Picks %>%
       filter(Pick > Picks_Drafted) %>%
@@ -297,6 +415,7 @@ repeat{
 }
 #
 
+
 combined_mocks <- map_dfr(paste0("Draft",1:Count,".csv"),readr::read_csv) 
 
 
@@ -311,5 +430,4 @@ combined_mocks_analysis <- combined_mocks %>%
             quantile10 = quantile(Pick, 0.9),
             drafted_in = n()) %>%
   arrange(mean_pick,median_pick,quantile90,quantile75,quantile25,quantile10,FFverse_OverallRank) %>%
-  filter(Position == "QB") %>%
   view()
